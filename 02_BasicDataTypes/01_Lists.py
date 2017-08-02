ac1f580fa826c2b2fea94f600ab727805c6f2147
Xiaoyi Lin <xxl163230@utdallas.edu>
2017-08-02 01:22:43 -0500

    final edition

--- 02_BasicDataTypes/01_Lists.py
+++ 02_BasicDataTypes/01_Lists.py
@@ -29,17 +29,17 @@ if __name__ == '__main__':
     for i in range(1, N+1):
         linput = input()
         s = linput.split()
-    	if s[0] == 'insert':
+        if s[0] == 'insert':
             l.insert(int(s[1]), int(s[2]))
         elif s[0] == 'print':
             print(l)
         elif s[0] == 'remove':
-        	l.remove(int(s[1]))
+            l.remove(int(s[1]))
         elif s[0] == 'append':
-        	l.append(int(s[1]))
+            l.append(int(s[1]))
         elif s[0] == 'sort':
-        	l.sort()
+            l.sort()
         elif s[0] == 'pop':
-        	l.pop()
+            l.pop()
         elif s[0] == 'reverse':
-        	l.reverse()
+            l.reverse()
